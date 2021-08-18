91ec77adcf4e76fa54dfe0f5a1f252cef61b0ddd
tsehlan <tsehlankhi@gmail.com>
2021-08-18 10:02:37 +0200

    make it better

--- index.js
+++ index.js
@@ -556,7 +556,7 @@ app.get('/find_webpage', function(req,res){
 const epub = require('epub-gen');//epub generator module
 
 app.use(express.json({limit: '50mb'})); //Used to parse JSON bodies
-app.use(express.urlencoded({limit: '50mb'})); //Parse URL-encoded bodies
+app.use(express.urlencoded({limit: '50mb',extended: true, parameterLimit: 1000000})); //Parse URL-encoded bodies
 
 // profile short link check
 app.post("/cook_epub", function(req, res){
